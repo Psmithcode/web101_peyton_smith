a204af5596f30e46816a6617d3d2533ca1f1c824
Psmithcode <peytonsmith53@gmail.com>
2022-08-07 14:04:57 -0500

    added functionality to contact page

--- Contactme/contact.js
+++ Contactme/contact.js
@@ -1,3 +1,10 @@
-const yesButton = document.getElementById("yesNoContainer")
-localStorage.setItem("yesnoButton", yesButton.elements['yesno'].value)
-console.log(yesButton)
+// const yesButton = document.getElementById("yesNoContainer")
+// localStorage.setItem("yesnoButton", yesButton.elements['yesno'].value)
+// console.log(yesButton)
+
+const placeForResults = document.getElementById("results")
+
+new URLSearchParams(window.location.search).forEach((value, name) => {
+placeForResults.append(`${name} : ${value}`)
+placeForResults.append(document.createElement("br"))
+})
